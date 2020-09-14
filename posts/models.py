40c94787b86f3597a795e69b30645d2278da6a7e
hyeoneedyou <hyeoneedyou@naver.com>
2020-09-15 03:07:11 +0900

    글자수 제한 해제

--- posts/models.py
+++ posts/models.py
@@ -9,7 +9,7 @@ class Post(models.Model):
     ]
     category = models.CharField(choices=POST_CATEGORY_CHOICES, max_length=300)
     title = models.CharField(max_length=50, null=False)
-    content = models.CharField(max_length=100, null=False)
+    content = models.TextField()
     image = models.ImageField(upload_to='images/', null=True)
     video = models.FileField(upload_to='videos/', null=True)
     created_at = models.DateTimeField(auto_now_add = True)
