cc641ca2f37e6cdda1e2a6295366e9be38611818
hyeoneedyou <hyeoneedyou@naver.com>
2020-09-15 01:42:16 +0900

    Post 모델 생성 + media 폴더 생성

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    POST_CATEGORY_CHOICES = [
+        ('주점', '주점'),
+        ('부스', '부스'),
+        ('공연', '공연'),
+        ('기타', '기타'),
+    ]
+    category = models.CharField(choices=POST_CATEGORY_CHOICES, max_length=300, null=True)
+    title = models.CharField(max_length=50, null=False)
+    content = models.CharField(max_length=100, null=False)
+    image = models.ImageField(upload_to='images/', null=True)
+    video = models.FileField(upload_to='videos/', null=True)
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
