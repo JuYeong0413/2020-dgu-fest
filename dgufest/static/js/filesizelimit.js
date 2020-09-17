260e23673479816eefdf2a6d982dcf8a8c5c278f
SJLEE316 <lsj8249@naver.com>
2020-09-17 21:24:24 +0900

    파일용량제한 문구 수정

--- dgufest/static/js/filesizelimit.js
+++ dgufest/static/js/filesizelimit.js
@@ -1,6 +1,6 @@
 function limitSize(input) {
     if (input.files[0].size > (1*1024*1024)) {
-        alert("파일 사이즈가 1mb를 넘습니다.");
+        alert("파일 크기가 1mb를 넘습니다. 1mb이하의 파일만 등록이 가능합니다.");
         input.value = null;
     }
 
