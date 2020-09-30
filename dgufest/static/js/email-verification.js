f1fb1e3b600ca94bb22ed4c0195c79f7b210f734
Juyeong Lee <2jy22@naver.com>
2020-10-01 06:00:21 +0900

    email verification

--- dgufest/static/js/email-verification.js
+++ dgufest/static/js/email-verification.js
@@ -1,7 +1,9 @@
 function verifyEmail() {
-    var email = document.getElementById('id_email').value;
-    if (!email.includes("@dgu.edu") || !email.includes("@dgu.ac.kr") || !email.includes("@dongguk.edu")) {
-        alert("재학생 인증을 위해 동국대학교 웹메일을 입력해주세요.");
-        return false;
-    } else return true;
+  var email = document.getElementById('id_email').value;
+  if (email.includes("@dgu.edu") || email.includes("@dgu.ac.kr") || email.includes("@dongguk.edu")) {
+    return true;
+  } else {
+    alert("재학생 인증을 위해 동국대학교 웹메일을 입력해주세요.");
+    return false;
+  }
 }
