98f1c858c438ad50fb5ecf947747665d96cba8e8
lee00096 <lee00096kr@gmail.com>
2020-10-01 14:36:06 +0900

    postlist, likelist users앱에이동

--- users/urls.py
+++ users/urls.py
@@ -7,4 +7,6 @@ app_name="users"
 urlpatterns = [
     path('mypage/', mypage, name="mypage"),
     path('password_change/', password_change, name="password_change"),
+    path('like_list/', like_list, name="like_list"),
+    path('postlist/', postlist, name="postlist"),
 ]
