94a89ff03fbfe214f7b5cd1d9dea9c88e29078d1
Juyeong Lee <2jy22@naver.com>
2020-10-04 00:36:35 +0900

    비밀번호 변경 부분 삭제, 데코레이터 추가

--- users/urls.py
+++ users/urls.py
@@ -5,7 +5,6 @@ from . import views
 app_name="users"
 urlpatterns = [
     path('mypage/', mypage, name="mypage"),
-    path('password_change/', password_change, name="password_change"),
     path('likes/', like_list, name="like_list"),
     path('posts/', postlist, name="postlist"),
 ]
--- users/views.py
+++ users/views.py
@@ -8,24 +8,6 @@ from posts.models import Like, Post
 def mypage(request):
     return render(request, 'users/mypage.html')
 
-def password_change(request):
-    context= {}
-    if request.method == "POST":
-        current_password = request.POST.get("origin_password")
-        user = request.user
-        if check_password(current_password,user.password):
-            new_password = request.POST.get("password1")
-            password_confirm = request.POST.get("password2")
-            if new_password == password_confirm:
-                user.set_password(new_password)
-                user.save()
-                auth.login(request,user)
-                return redirect("users:myblog")
-            else:
-                context.update({'error':"새로운 비밀번호를 다시 확인해주세요."})
-    else:
-        context.update({'error':"현재 비밀번호가 일치하지 않습니다."})
-    return render(request, 'users/password_change.html')
 
 @login_required
 def like_list(request):
@@ -33,6 +15,7 @@ def like_list(request):
     return render(request, 'users/like_list.html', {'likes': likes})
 
 
+@login_required
 def postlist(request):
     user = request.user
     context = {'posts' : Post.objects.filter(user=user)}
