4bfaca01492f28eb14d0313aef1111e922ad47df
surfing2003 <surfing2003@naver.com>
2020-10-03 17:26:55 +0900

    전화번호 중복문제 해결

--- users/forms.py
+++ users/forms.py
@@ -9,6 +9,12 @@ class MyCustomSignupForm(SignupForm):
     student_id = forms.IntegerField(label='학번', widget=forms.TextInput(attrs={'placeholder': '10자리 학번'}))
     phone_number = forms.IntegerField(label='핸드폰번호', widget=forms.TextInput(attrs={'placeholder': '숫자만 입력해주세요.'}))
 
+    def clean_phone_number(self):
+        phone_number = self.cleaned_data.get('phone_number')
+        if Profile.objects.filter(phone_number=phone_number).exists():
+            raise forms.ValidationError('이미 존재하는 전화번호 입니다.')
+        return phone_number
+
     def save(self, request):
         user = super(MyCustomSignupForm, self).save(request)
         user.profile.name = self.cleaned_data['name']
@@ -16,3 +22,4 @@ class MyCustomSignupForm(SignupForm):
         user.profile.phone_number = self.cleaned_data['phone_number']
         user.save()
         return user
+
