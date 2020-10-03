f921f9ba19952e620b810059d1234314fdca3e8a
ygk313 <50168972+ygk313@users.noreply.github.com>
2020-10-03 21:02:59 +0900

    함수 이름 수정

--- dgufest/static/js/chk_title.js
+++ dgufest/static/js/chk_title.js
@@ -1,4 +1,4 @@
-function verifyStudentId() {
+function checktitle() {
     var titleId = document.getElementById('title_id').value;
     if (titleId.length > 15) { 
       alert("공백포함 최대 15글자 입력가능합니다!");
