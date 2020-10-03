1b1a9c402861ff1be4be9555374c4038bf7f04b8
Juyeong Lee <2jy22@naver.com>
2020-10-03 22:44:00 +0900

    회원가입 학번 중복 체크

--- users/forms.py
+++ users/forms.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 
 class MyCustomSignupForm(SignupForm):
     name = forms.CharField(max_length=10, label='이름', widget=forms.TextInput(attrs={'placeholder': '이름'}))
-    student_id = forms.IntegerField(label='학번', widget=forms.TextInput(attrs={'placeholder': '10자리 학번'}))
+    student_id = forms.IntegerField(label='학번(10자리)', widget=forms.TextInput(attrs={'placeholder': '10자리 학번'}))
     phone_number = forms.CharField(label='핸드폰번호', widget=forms.TextInput(attrs={'placeholder': '숫자만 입력해주세요.'}))
 
     def clean_phone_number(self):
@@ -14,7 +14,7 @@ class MyCustomSignupForm(SignupForm):
         if len(phone_number) != 11:
             raise forms.ValidationError('전화번호를 다시 확인해주세요.')
         if Profile.objects.filter(phone_number=phone_number).exists():
-            raise forms.ValidationError('이미 존재하는 전화번호 입니다.')
+            raise forms.ValidationError('이미 가입된 전화번호입니다.')
         return phone_number
 
     def clean_student_id(self):
@@ -23,7 +23,10 @@ class MyCustomSignupForm(SignupForm):
             raise forms.ValidationError("10자리의 학번을 입력해주세요.")
         if not str(student_id).startswith('20'):
             raise forms.ValidationError("학번을 확인해주세요.")
+        if Profile.objects.filter(student_id=student_id).exists():
+            raise forms.ValidationError('이미 가입된 학번입니다.')
         return student_id
+
     def save(self, request):
         user = super(MyCustomSignupForm, self).save(request)
         user.profile.name = self.cleaned_data['name']
