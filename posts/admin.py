5da2a41e9f9fd686a39e0f2b83d918ba27571b3a
Juyeong Lee <2jy22@naver.com>
2020-10-04 01:12:17 +0900

    Admin list update

--- posts/admin.py
+++ posts/admin.py
@@ -1,19 +1,35 @@
 from django.contrib import admin
 from .models import *
 
-# Register your models here.
-
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = (
         "id",
         "user",
+        "get_user_name",
+        "get_student_id",
+        "get_phone_number",
         "category",
         "title",
-        "content",
         "mediafile",
-        "like_count",
+        "number_of_likes",
     )
+
+    def number_of_likes(self, obj):
+        return obj.like_count
+    number_of_likes.admin_order_field = 'like'
+
+    def get_user_name(self, obj):
+        return obj.user.profile.name
+    get_user_name.short_description = 'User Name'
+
+    def get_student_id(self, obj):
+        return obj.user.profile.student_id
+    get_student_id.short_description = 'Student Id'
+
+    def get_phone_number(self, obj):
+        return obj.user.profile.phone_number
+    get_phone_number.short_description = 'Phone Number'
 
 
 @admin.register(Like)
