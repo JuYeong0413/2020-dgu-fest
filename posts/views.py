cf52ef7afc9aca197441606bfd66f1d9e9042625
Juyeong Lee <2jy22@naver.com>
2020-11-02 02:30:50 +0900

    Revert "투표 집계 완료되어 좋아요 기능 복구"
    This reverts commit e3e6dc415b60239d4dda04013d57a9e97447ad70.

--- posts/views.py
+++ posts/views.py
@@ -108,17 +108,24 @@ def show(request, id):
 def post_like(request, post_id):
     post = get_object_or_404(Post, pk=post_id)
 
-    post_like, post_like_created = post.like_set.get_or_create(user=request.user)
+    now = datetime.datetime.now()
+    voting_end_date = datetime.datetime(2020, 11, 2, 0, 0, 0, 0)
 
-    if not post_like_created:
-        post_like.delete()
-        heart_icon = '<i class="far fa-heart"></i>'
+    if now > voting_end_date:
+        # 현재 시간이 투표 마감 시간 이후인 경우
+        return HttpResponseBadRequest('timeout')
     else:
-        heart_icon = '<i class="fas fa-heart"></i>'
+        post_like, post_like_created = post.like_set.get_or_create(user=request.user)
 
-    context = {
-        'heart_icon': heart_icon,
-        'like_count': post.like_count,
-    }
+        if not post_like_created:
+            post_like.delete()
+            heart_icon = '<i class="far fa-heart"></i>'
+        else:
+            heart_icon = '<i class="fas fa-heart"></i>'
 
-    return HttpResponse(json.dumps(context), content_type="application/json")
+        context = {
+            'heart_icon': heart_icon,
+            'like_count': post.like_count,
+        }
+
+        return HttpResponse(json.dumps(context), content_type="application/json")
