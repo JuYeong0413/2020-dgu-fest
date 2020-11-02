0cbdea125663f42568a12be70a2285ee17ee76fb
hyeoneedyou <hyeoneedyou@naver.com>
2020-11-02 13:41:06 +0900

    수상작 수정

--- dgufest/settings.py
+++ dgufest/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'tf6b*ea^k1*-e^h(g$gh*w7!n!@3%j)p3502&&e1!!)ydo)ev('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
