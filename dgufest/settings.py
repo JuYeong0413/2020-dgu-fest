b0fc10efb0a73cd53fcda45bc6d61ef128f8d4c6
Juyeong Lee <2jy22@naver.com>
2020-11-03 14:28:29 +0900

    +

--- dgufest/settings.py
+++ dgufest/settings.py
@@ -10,17 +10,19 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
-import os
+import os, json
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+CREDENTIAL_FILE = os.path.join(BASE_DIR, 'credentials.json')
+credentials = json.loads(open(CREDENTIAL_FILE).read())
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'tf6b*ea^k1*-e^h(g$gh*w7!n!@3%j)p3502&&e1!!)ydo)ev('
+SECRET_KEY = credentials["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
